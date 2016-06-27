88e7776eb16a95cb7eefb4f00a39580f66e2596c
Patrick J. Wu <wotingwu@live.com>
2016-06-27 17:00:35 +0800

    New Code Base

---
+++ Core/network.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Core
+{
+    public class Network
+    {
+        /// <summary>
+        /// Reach For Internet Files
+        /// </summary>
+        /// <param name="url">URL of the file</param>
+        /// <param name="num">0 for just reach the message, 1 for download to local folder Data</param>
+        public async static Task DownloadFile(string url, int num)
+        {
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(new Uri(url));
+            if(num == 1)
+            {
+                string filename = url.Split("/".ToCharArray()).Last();
+                StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+                StorageFile storageFile = await storageFolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+                var streamout = await storageFile.OpenAsync(FileAccessMode.ReadWrite);
+                using (var outputStream = streamout.GetOutputStreamAt(0))
+                {
+                    using (var dataWriter = new Windows.Storage.Streams.DataWriter(outputStream))
+                    {
+                        var DataString = await response.Content.ReadAsStringAsync();
+                        dataWriter.WriteString(DataString);
+                        await dataWriter.StoreAsync();
+                        await outputStream.FlushAsync();
+                    }
+                }
+                streamout.Dispose();
+            }
+        }
+    }
+}
