8cd6d9f8ca143a5844ffb47e082d3205ee5cddfc
Patrick J. Wu <wotingwu@live.com>
2016-07-16 15:05:41 +0800

    NetworkStatus

--- Core/network.cs
+++ Core/network.cs
@@ -2,24 +2,37 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Windows.Storage;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
 using Windows.Web.Http;
 
-namespace Core
+namespace Core.Network
 {
-    public class Network
+    public enum Response
+    {
+        DownloadToLocalFolder = 1,
+        DownloadToRemoteFolder = 2,
+        DownloadToTemporaryFolder = 3,
+    }
+    public struct StatusCode
+    {
+        public string code { get; set; }
+        public string status { get; set; }
+    }
+    public struct Status
+    {
+        public Brush color { get; set; }
+        public string status { get; set; }
+        public string code { get; set; }
+        public string details { get; set; }
+
+    }
+    /// <summary>
+    /// URL Praser
+    /// </summary>
+    public class UrlPhraser
     {
         private static StorageFolder storageFolder;
-        public enum Response
-        {
-            DownloadToLocalFolder = 1,
-            DownloadToRemoteFolder = 2,
-            DownloadToTemporaryFolder = 3,
-        }
-        public struct Status
-        {
-            public string code { get; set; }
-           public string status { get; set; }
-        }
 
         /// <summary>
         /// Reach URL and get response
@@ -90,14 +103,84 @@ namespace Core
         /// </summary>
         /// <param name="url">URL</param>
         /// <returns></returns>
-        public async static Task<Status> ReachStatusCode(string url)
+        public async static Task<StatusCode> ReachStatusCode(string url)
         {
-            Status code = new Status();
+            StatusCode code = new StatusCode();
             HttpResponseMessage httpResponse = await ReachHeader(url);
             string[] text = httpResponse.ToString().Split(Convert.ToChar(","));
             code.code = (text[0].Split(Convert.ToChar(":")))[1].Substring(1);
             code.status = (text[1].Split(Convert.ToChar(":")))[1].Replace(Convert.ToChar("'"), Convert.ToChar(" ")).Trim();
             return code;
+        }
+    }
+    /// <summary>
+    /// Get the status of the network.
+    /// </summary>
+    public class NetworkStatus: UrlPhraser
+    {
+        public async Task<Status> GetStatus(string website)
+        {
+            Status status = new Status();
+            StatusCode code = await ReachStatusCode(website);
+            status.details = code.status;
+            status.code = code.code;
+            if (code.code.StartsWith("1"))
+            {
+                status.status = "On Process";
+                status.color = GetStatusColor("b");
+            }
+            else if (code.code.StartsWith("2"))
+            {
+                status.status = "Operational";
+                status.color = GetStatusColor("g");
+            }
+            else if (code.code.StartsWith("3"))
+            {
+                status.status = "Redirection Needed";
+                status.color = GetStatusColor("y");
+            }
+            else if (code.code.StartsWith("4"))
+            {
+                status.status = "Client Error";
+                status.color = GetStatusColor("r");
+            }
+            else if (code.code.StartsWith("5"))
+            {
+                status.status = "Server Error";
+                status.color = GetStatusColor("r");
+            }
+            else
+            {
+                status.status = "Unknown";
+                status.color = GetStatusColor("b");
+            }
+            return status;
+        }
+        /// <summary>
+        /// Set the proper color of network status
+        /// </summary>
+        /// <param name="color">Color indicator:r as red,g as green, b as blue, y as yellow</param>
+        /// <returns></returns>
+        public Brush GetStatusColor(string color)
+        {
+            Color c;
+            switch (color)
+            {
+                case "r":
+                    c = Colors.Red;
+                    break;
+                case "g":
+                    c = Colors.LightGreen;
+                    break;
+                case "y":
+                    c = Colors.Yellow;
+                    break;
+                case "b":
+                    c = Colors.Blue;
+                    break;
+            }
+            Brush brush = new SolidColorBrush(c);
+            return brush;
         }
     }
 }
