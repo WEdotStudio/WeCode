20ea517a2b040cd783202f29f8765ea27d9cebb2
Patrick J. Wu <wotingwu@live.com>
2016-07-16 13:28:46 +0800

    Network Core Modification 2

--- Core/network.cs
+++ Core/network.cs
@@ -1,46 +1,84 @@
 ﻿using System;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Windows.Storage;
+using Windows.Web.Http;
 
 namespace Core
 {
     public class Network
     {
+        private static StorageFolder storageFolder;
         public enum Response
         {
-            Null =0,
-            Download=1
+            DownloadToLocalFolder = 1,
+            DownloadToRemoteFolder = 2,
+            DownloadToTemporaryFolder = 3,
+        }
+
+        
+        public async static Task<HttpResponseMessage> ReachURL(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(new Uri(url));
+            return response;
         }
         /// <summary>
-        /// reach internet files and modify it.
+        /// Get File And Download it.
         /// </summary>
         /// <param name="url">URL of the file</param>
-        /// <param name="num">Ways to operate the file</param>
-        public async static Task<HttpResponseMessage> ReachFile(string url, Response way)
+        /// <param name="way">Ways to deal with the file</param>
+        public async static Task ReachFile(string url, Response way)
         {
-            var client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync(new Uri(url));
-            if(way == Response.Download)
+            HttpResponseMessage response = await ReachURL(url);
+
+            string filename = url.Split("/".ToCharArray()).Last();
+            switch (way)
             {
-                string filename = url.Split("/".ToCharArray()).Last();
-                StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
-                StorageFile storageFile = await storageFolder.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
-                var streamout = await storageFile.OpenAsync(FileAccessMode.ReadWrite);
-                using (var outputStream = streamout.GetOutputStreamAt(0))
-                {
-                    using (var dataWriter = new Windows.Storage.Streams.DataWriter(outputStream))
-                    {
-                        var DataString = await response.Content.ReadAsStringAsync();
-                        dataWriter.WriteString(DataString);
-                        await dataWriter.StoreAsync();
-                        await outputStream.FlushAsync();
-                    }
-                }
-                streamout.Dispose();
+                case Response.DownloadToLocalFolder:
+                    storageFolder = ApplicationData.Current.LocalFolder;
+                    break;
+                case Response.DownloadToRemoteFolder:
+                    storageFolder = ApplicationData.Current.RoamingFolder;
+                    break;
+                case Response.DownloadToTemporaryFolder:
+                    storageFolder = ApplicationData.Current.TemporaryFolder;
+                    break;
             }
-            return response;
-        }   
+
+            StorageFile storageFile = await storageFolder.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
+            var streamout = await storageFile.OpenAsync(FileAccessMode.ReadWrite);
+            using (var outputStream = streamout.GetOutputStreamAt(0))
+            {
+                using (var dataWriter = new Windows.Storage.Streams.DataWriter(outputStream))
+                {
+                    var DataString = await response.Content.ReadAsStringAsync();
+                    dataWriter.WriteString(DataString);
+                    await dataWriter.StoreAsync();
+                    await outputStream.FlushAsync();
+                }
+            }
+            streamout.Dispose();
+
+        }
+
+        /// <summary>
+        /// Get Header 
+        /// </summary>
+        /// <param name="url">URL of the file</param>
+        public async static Task<HttpResponseMessage> ReachHeader(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage httpResponse = new HttpResponseMessage();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, new Uri(url));
+            httpResponse = await httpClient.SendRequestAsync(request);
+            return httpResponse;
+        }
+
+        public async static Task<string> ReachStatusCode(string url)
+        {
+            HttpResponseMessage httpResponse = await ReachHeader(url);
+
+        }
     }
 }
