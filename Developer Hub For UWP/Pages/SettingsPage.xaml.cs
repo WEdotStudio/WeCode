06fea4f54c1a9f2fad36af2121544c95856e0469
Patrick J. Wu <wotingwu@live.com>
2016-07-21 10:15:02 +0800

    Settings:Notification Disable

--- Developer Hub For UWP/Pages/SettingsPage.xaml.cs
+++ Developer Hub For UWP/Pages/SettingsPage.xaml.cs
@@ -5,14 +5,20 @@ using Windows.UI.Xaml.Controls;
 
 namespace Developer_Hub_For_UWP.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class SettingsPage : Page
     {
+        private ApplicationDataContainer _localSettings;
         public SettingsPage()
         {
+            _localSettings = ApplicationData.Current.LocalSettings;
+            var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
+
             this.InitializeComponent();
+
+            bool PopupDisabled = Convert.ToBoolean(_localSettings.Containers["Settings"].Values["IsUpdatePopupDisabled"]);
+            txt.IsOn = PopupDisabled;
+            txt.OnContent = loader.GetString("on");
+            txt.OffContent = loader.GetString("off");
         }
 
         private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
@@ -34,7 +40,15 @@ namespace Developer_Hub_For_UWP.Pages
 
         private void txt_Toggled(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-
+            switch (txt.IsOn)
+            {
+                case true:
+                    _localSettings.Containers["Settings"].Values["IsUpdatePopupDisabled"] = true;
+                    break;
+                case false:
+                    _localSettings.Containers["Settings"].Values["IsUpdatePopupDisabled"] = false;
+                    break;
+            } 
         }
     }
 }
