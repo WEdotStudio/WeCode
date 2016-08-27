c1e48c3c6246fc9fd06839ebf4add9e393e8f5f8
Patrick J. Wu <wotingwu@live.com>
2016-08-27 12:58:44 +0800

    WeCode 3.0 Preperation 2

---
+++ WeCode/Pages/Home.xaml.cs
@@ -0,0 +1,138 @@
+﻿using Core.DataModel;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Windows.ApplicationModel.Core;
+using Windows.Security.ExchangeActiveSyncProvisioning;
+using Windows.Storage;
+using Windows.System.Profile;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using static Core.DataModel.insideten;
+
+namespace Developer_Hub_For_UWP.Pages
+{
+
+    public sealed partial class Home : Page
+    {
+        public ApplicationDataContainer _localSettings;
+        public Home()
+        {
+            this.InitializeComponent();
+            _localSettings = ApplicationData.Current.LocalSettings;
+            
+            var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
+            title.Text = loader.GetString("LBI");
+
+            UpdateBuildInfo();
+            InitializeIconHistoryList();
+        }
+        
+        private async void InitializeIconHistoryList()
+        {
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFile storageFile = await storageFolder.CreateFileAsync("history_icon.log", CreationCollisionOption.OpenIfExists);
+            using (var storageStream = await storageFile.OpenReadAsync())
+            {
+                using (Stream stream = storageStream.AsStreamForRead())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        List<Character> Items = new List<Character>();
+
+                        while (!reader.EndOfStream)
+                        {
+                            var line = reader.ReadLine();
+                            var values = line.Split(',');
+                            Items.Add(new Character{ Font = values[0], Char = values[1], UnicodeIndex=Convert.ToInt32(values[2])});
+                        }
+                        Items.Reverse();
+                        if(Items.Count == 0)
+                        {
+                            noH.Visibility = Visibility.Visible;
+                        }
+                        else 
+                        {
+                            noH.Visibility = Visibility.Collapsed;
+                            if (Items.Count > 8) Items = Items.GetRange(0, 8);
+                        }
+                       
+                        gridView.ItemsSource = Items;
+
+
+                        reader.Dispose();
+                    }
+                    stream.Dispose();
+                }
+                storageStream.Dispose();
+            }
+        }
+        private async void UpdateBuildInfo()
+        {
+            //System Version
+            string sv = AnalyticsInfo.VersionInfo.DeviceFamilyVersion;
+            ulong v = ulong.Parse(sv);
+            ulong v3 = (v & 0x00000000FFFF0000L) >> 16;
+            bu.Text = $"{v3}";
+
+            //System Info
+            EasClientDeviceInformation eas = new EasClientDeviceInformation();
+            ma.Text = eas.SystemManufacturer+" "+ eas.SystemProductName;
+
+            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+            StorageFile jsonFile = await localFolder.GetFileAsync("api.json");
+            var jsonString = await FileIO.ReadTextAsync(jsonFile);
+            
+            RootObject data = JsonConvert.DeserializeObject<RootObject>(jsonString);
+            Build_v.Text = data.@internal.build;
+        }
+
+        private void StackPanel_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ItemsWrapGrid MyItemsPanel = (ItemsWrapGrid)gridView.ItemsPanelRoot;
+            MyItemsPanel.ItemWidth = (e.NewSize.Width - 10) / 8;
+        }
+
+        private async void gridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
+            Character output = e.ClickedItem as Character;
+            var currentAV = ApplicationView.GetForCurrentView();
+            var newAV = CoreApplication.CreateNewView();
+            await newAV.Dispatcher.RunAsync(
+                            CoreDispatcherPriority.Normal,
+                            async () =>
+                            {
+                                var newWindow = Window.Current;
+                                var newAppView = ApplicationView.GetForCurrentView();
+
+                                newAppView.Title = loader.GetString("Details");
+
+                                var frame = new Frame();
+                                frame.Navigate(typeof(Browser), output);
+                                newWindow.Content = frame;
+                                newWindow.Activate();
+
+                                await ApplicationViewSwitcher.TryShowAsStandaloneAsync(
+                                    newAppView.Id,
+                                    ViewSizePreference.UseMinimum,
+                                    currentAV.Id,
+                                    ViewSizePreference.UseMinimum);
+                            });
+        }
+
+        private void TextBlock_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Page4), Frame);
+        }
+
+        private void TextBlock_Tapped_1(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Page1), Frame);
+        }
+    }
+   
+}
---
+++ WeCode/Pages/Page4.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using Windows.Storage;
+using Windows.UI.Xaml.Controls;
+using static Core.DataModel.insideten;
+
+namespace Developer_Hub_For_UWP.Pages
+{
+    public sealed partial class Page4 : Page
+    {
+        public Page4()
+        {
+            this.InitializeComponent();
+
+            var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
+            title.Text = loader.GetString("LBI");
+
+            UpdateData();
+        }
+
+        private async void UpdateData()
+        {
+            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+            StorageFile jsonFile = await localFolder.GetFileAsync("api.json");
+            var jsonString = await FileIO.ReadTextAsync(jsonFile);
+
+            RootObject data = JsonConvert.DeserializeObject<RootObject>(jsonString);
+
+            Build_v.Text = data.@internal.build;
+
+            pcwrp_b.Text = data.pcwrp.build;
+            pcwrp_v.Text = data.pcwrp.version;
+            pcwrp_m.NavigateUri = new Uri(add_format(data.pcwrp.more));
+            pcwrp_r.Text = data.pcwrp.release_date;
+
+            pcwif_b.Text = data.pcwif.build;
+            pcwif_v.Text = data.pcwif.version;
+            pcwif_m.NavigateUri = new Uri(add_format(data.pcwif.more));
+            pcwif_r.Text = data.pcwif.release_date;
+
+            pcwis_b.Text = data.pcwis.build;
+            pcwis_v.Text = data.pcwis.version;
+            pcwis_m.NavigateUri = new Uri(add_format(data.pcwis.more));
+            pcwis_r.Text = data.pcwis.release_date;
+
+            mowrp_b.Text = data.mowrp.build;
+            mowrp_v.Text = data.mowrp.version;
+            mowrp_m.NavigateUri = new Uri(add_format(data.mowrp.more));
+            mowrp_r.Text = data.mowrp.release_date;
+
+            mowif_b.Text = data.mowif.build;
+            mowif_v.Text = data.mowif.version;
+            mowif_m.NavigateUri = new Uri(add_format(data.mowif.more));
+            mowif_r.Text = data.mowif.release_date;
+
+            mowis_b.Text = data.mowis.build;
+            mowis_v.Text = data.mowis.version;
+            mowis_m.NavigateUri = new Uri(add_format(data.mowis.more));
+            mowis_r.Text = data.mowis.release_date;
+
+        }
+      
+    }
+}
